f0b0486baab55d35bbba95cca46cc703880a5b04
jamesmckechnie <34271054+jamesmckechnie@users.noreply.github.com>
2023-12-19 09:16:33 +0000

    crd journey

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,25 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+router.post(`/task-based-routing`, function (req, res) {
+	const navRoute = req.session.data['navOption'];
+	if (navRoute === 'single-entry') {
+		res.redirect(`v5/overview`);
+	}
+	else if (navRoute === 'adjustments') {
+		res.redirect(`v5/adjustments`);
+	}
+	else if (navRoute === 'crd') {
+		res.redirect(`v5/calculate-release-dates/check-sentence-and-offence-information`);
+	}
+	else if (navRoute === 'court-cases') {
+		res.redirect(`v5/court-cases`);
+	}
+	else if (navRoute === 'crd-lookup') {
+		res.redirect(`v5/release-dates`);
+	}
+	else {
+		res.redirect(`v5/overview`);
+	}
+  });
