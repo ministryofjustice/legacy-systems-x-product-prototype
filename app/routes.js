6a581ce6fb0158936bb781287d44b329e81fd5a1
jamesmckechnie <34271054+jamesmckechnie@users.noreply.github.com>
2024-05-20 15:03:38 +0100

    printed slip changes

--- app/routes.js
+++ app/routes.js
@@ -49,11 +49,42 @@ router.post(`/associate-doc-choice`, function (req, res) {
     const prototypeVersion = req.params.prototypeVersion 
     res.redirect(`/${prototypeVersion}/calculate-release-dates/calculation-complete?newCalculation=true`)
 })
+
+
+ 
+router.post(`/:prototypeVersion/approved-dates-question`, function (req, res) {
+	const prototypeVersion = req.params.prototypeVersion
+	var navRoute = req.session.data['approvedDatesQuestion'];
+	if (navRoute === 'yes') {
+		res.redirect(`/${prototypeVersion}/calculate-release-dates/select-which-approved-dates`);
+	}
+	else if (navRoute === 'no') {
+		res.redirect(`/${prototypeVersion}/calculate-release-dates/calculaion-complete`);
+	} 
+	else {
+		res.redirect(`/${prototypeVersion}/calculate-release-dates/calculaion-complete`);
+	}
+  });
+
+ 
+  router.post(`/:prototypeVersion/which-dates-to-add`, function (req, res) {
+	const prototypeVersion = req.params.prototypeVersion
+	var dateSelect = req.session.data['date-delect'];
+	if (dateSelect == 'APD') {
+		res.redirect(`/${prototypeVersion}/calculate-release-dates/enter-APD`);
+	}
+	else if (dateSelect == 'HDCAD') {
+		res.redirect(`/${prototypeVersion}/calculate-release-dates/enter-HDCAD.html`);
+	} 
+	else if (dateSelect == 'ROTL') {
+		res.redirect(`/${prototypeVersion}/calculate-release-dates/enter-ROTL.html`);
+	} 
+	else {
+		res.redirect(`/${prototypeVersion}/calculate-release-dates/enter-APD`);
+	}
+  });
+ 
+ 
  
 
-  router.get('/:prototypeVersion/view-court-case-detail', function(req, res) {
-    const prototypeVersion = req.params.prototypeVersion
-    const courtCaseIndex = Number(req.query.courtCaseIndex) + 1
-    console.log('Court case index: ' + courtCaseIndex)
-    res.redirect(`/${prototypeVersion}/court-cases/court-case-detail`)
-})
+ 
